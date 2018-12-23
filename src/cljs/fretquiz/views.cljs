823b5bc29d6101ecb52ce2db388e045f59adc40c
Fredrik Roman <fredrik.roman@trioptima.com>
2018-12-23 09:25:50 -0500

    New colors

--- src/cljs/fretquiz/views.cljs
+++ src/cljs/fretquiz/views.cljs
@@ -26,6 +26,15 @@
                               66.629
                               68.502])
 
+(def color {:tamarillo    "#9b2117"
+            :thunder      "#221e21"
+            :black        "#000000"
+            :punch        "#da3732"
+            :pavlova      "#d7c899"
+            :soya-bean    "#5c5445"
+            :indian-khaki "#c3b092"
+            :white        "#efefe7"})
+
 (defn fret-x-position [fret-nr]
   (* 10
      (get fret->distance-from-nut (dec fret-nr))))
@@ -80,7 +89,7 @@
                           (M 0 y-position)
                           (L fretboard-length y-position)]))])
 
-(defn pointer [{:keys [y-padding fretboard-width nr-of-strings string-to-guess fret-to-guess]}]
+(defn pointer [{:keys [y-padding fretboard-width nr-of-strings string-to-guess fret-to-guess pointer-color]}]
   [:g.guess-pointer
    (let [x-position (- (fret-x-position fret-to-guess) 10)
          y-position (string-y-position y-padding fretboard-width nr-of-strings (dec string-to-guess))]
@@ -88,7 +97,7 @@
                :y      y-position
                :width  12
                :height 10
-               :fill   "black"}])])
+               :fill   pointer-color}])])
 
 (defn fretboard [{:keys [length width x y string-y-offset fretboard-color] :as ctx}]
   (let [{:keys [nr-of-strings nr-of-frets]} @(re-frame/subscribe [::subs/fretboard])
@@ -115,7 +124,18 @@
      [fret-nr-hints ctx]
      [pointer ctx]]))
 
-(defn head-strings [{:keys [length fretboard-width fretboard-y string-y-offset string-color nut-color]} nr-of-strings]
+(defn nut [{:keys [nut-color string-color]} x y]
+  (let [attrs {:cx           x
+               :cy           (- y 10)
+               :stroke       nut-color
+               :stroke-width 2
+               :fill         "none"}]
+    [:g
+     [:circle (assoc attrs :r 10)]
+     [:circle (assoc attrs :r 6 :stroke string-color :stroke-width 1)]
+     [:circle (assoc attrs :r 2)]]))
+
+(defn head-strings [{:keys [length fretboard-width fretboard-y string-y-offset string-color] :as ctx} nr-of-strings]
   [:g
    (for [string-nr (range nr-of-strings)]
      (let [y-start-position (+ fretboard-y
@@ -130,12 +150,7 @@
                                  :stroke       string-color}
                            (M length y-start-position)
                            (L x-position y-end-position)]
-                          [:circle {:cx           x-position
-                                    :cy           (- y-end-position 10)
-                                    :r            10
-                                    :stroke       nut-color
-                                    :stroke-width 1
-                                    :fill         "none"}]]))])
+                          [nut ctx x-position y-end-position]]))])
 
 (defn bridge [{:keys [length fretboard-y fretboard-width bridge-width bridge-color]}]
   [:line {:stroke       bridge-color
@@ -145,10 +160,10 @@
           :x2           (- length (/ bridge-width 2))
           :y2           (+ fretboard-y fretboard-width)}])
 
-(defn head-shape [{:keys [length width bridge-width fretboard-color]}]
+(defn head-shape [{:keys [length width bridge-width head-color]}]
   (let [length (- length bridge-width)]
     [path {:stroke "none"
-           :fill   fretboard-color}
+           :fill   head-color}
      (M 5 0)
      (Q 0 (/ width 2) 5 width)
      (L (- length 30) (- width 10))
@@ -170,9 +185,9 @@
      [head-strings ctx nr-of-strings]]))
 
 (defn balalaika []
-  (let [fretboard-width  90
+  (let [fretboard-width  110
         fretboard-length 700
-        head-width       150
+        head-width       (+ fretboard-width 60)
         head-length      300
         fretboard-y      (/ (- head-width fretboard-width) 2)
         string-y-offset  10
@@ -181,12 +196,14 @@
         ctx              {:fretboard-width fretboard-width
                           :fretboard-y     fretboard-y
                           :string-y-offset string-y-offset
-                          :fret-color      "red"
-                          :fretboard-color "grey"
-                          :string-color    "red"
-                          :bridge-color    "red"
-                          :nut-color       "red"
-                          :fret-hint-color "red"}]
+                          :fret-color      (color :soya-bean)
+                          :fretboard-color (color :indian-khaki)
+                          :head-color      (color :tamarillo)
+                          :string-color    (color :white)
+                          :bridge-color    (color :thunder)
+                          :nut-color       (color :thunder)
+                          :fret-hint-color (color :soya-bean)
+                          :pointer-color   (color :punch)}]
     [:svg {:width    balalaika-length
            :height   balalaika-width
            :view-box (str "0 0 " balalaika-length " " balalaika-width)}
@@ -207,7 +224,7 @@
      (map (fn [note]
             ^{:key note} [:button {:on-click #(re-frame/dispatch [::events/answer note])
                                    :style    {:background "none"
-                                              :border     "5px solid red"
+                                              :border     (str "5px solid " (color :punch))
                                               :font-size  "24px"}}
                           note])
           notes)]))
@@ -221,7 +238,19 @@
 
 (defn main-panel []
   [:div
-   [:h1 "Fretquiz"]
+   [:h1 "FretQuiz"]
+   (for [[color-name color-code] (sort (fn [[_ c1] [_ c2]]
+                                         (< (apply + (map #(js/parseInt % 16)
+                                                          (re-seq #".{1,4}" (.substring c1 1 7))))
+                                            (apply + (map #(js/parseInt % 16)
+                                                          (re-seq #".{1,4}" (.substring c2 1 7))))))
+                                       color)]
+     ^{:key color-name} [:div
+                         [:span {:style {:display          "inline-block"
+                                         :width            "10px"
+                                         :height           "10px"
+                                         :background-color color-code}}]
+                         [:span " " color-name]])
    [balalaika]
    [alternative-buttons]
    [result]])
